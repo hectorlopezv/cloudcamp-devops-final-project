0a10438128e9d779a80e4653354739f9602b80fe
hector.lopez <hectorvmlopez@gmail.com>
2024-06-10 10:19:47 -0500

    adding terraform loc

--- modules/EC2/main.tf
+++ modules/EC2/main.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "aws_app_server_1" {
       ami = "ami-0bb84b8ffd87024d8"
     instance_type = var.instance_type
     availability_zone = var.availability_zone
-    user_data = templatefile("user_data.tpl", { efs_id = var.efs_id })
+    # user_data = templatefile("user_data.tpl", { efs_id = var.efs_id })
 
       subnet_id     = var.subnet_id_1
   security_groups = [var.ec2_sg_id]
@@ -18,7 +18,7 @@ resource "aws_instance" "aws_app_server_2" {
       ami = "ami-0bb84b8ffd87024d8"
     instance_type = var.instance_type
     availability_zone = var.availability_zone
-     user_data = templatefile("user_data.tpl", { efs_id = var.efs_id })
+    #  user_data = templatefile("user_data.tpl", { efs_id = var.efs_id })
 
   subnet_id     = var.subnet_id_2
   security_groups = [var.ec2_sg_id]    
